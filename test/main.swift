be600d86e7857294fa0ada290436eb33041b113f
Noam Sellam <nompops@icloud.com>
2017-06-18 16:39:58 +0300

    Initial Commit

---
+++ test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test
+//
+//  Created by Noam Sellam on 18/06/2017.
+//  Copyright © 2017 Noam Sellam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
